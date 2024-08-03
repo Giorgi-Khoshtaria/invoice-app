d5f46c2980295276fb33b492bdfc33b6c8c4d620
giorgi khoshtaria <99177635+g1org11@users.noreply.github.com>
2024-08-03 16:35:53 +0400

    add clearContent component

--- src/conponents/Pages/Content.tsx
+++ src/conponents/Pages/Content.tsx
@@ -1,10 +1,12 @@
 import React from "react";
-import FilterAndNew from "../sub-components/FilterAndNew";
+import FilterAndNew from "../sub-components/FilterAndNew/FilterAndNew";
+import ClearContent from "../sub-components/ClearContent.tsx/ClearContent";
 
 function Content() {
   return (
     <div className=" w-full px-[20px] ">
       <FilterAndNew />
+      <ClearContent />
     </div>
   );
 }
---
+++ src/conponents/sub-components/ClearContent.tsx/ClearContent.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import illustration from "/assets/illustration-empty.svg";
+
+function ClearContent() {
+  return (
+    <div className=" flex items-center justify-center mt-[42px]">
+      <div className="flex items-center flex-col text-center">
+        <img src={illustration} alt="" />
+        <h2 className=" text-chineesBlack text-2xl font-bold mt-[42px] mb-[22px]">
+          There is nothing here
+        </h2>
+        <p className=" w-[230px] text-gray  font-[13px] tracking-[-0.1px]">
+          Create an invoice by clicking the New button and get started
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default ClearContent;
